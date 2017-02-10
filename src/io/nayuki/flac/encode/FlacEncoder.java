62c4d5b15b701f2a6a8c6fc219f0a5a7b6557648
Project Nayuki <me@nayuki.io>
2017-02-10 18:51:19 +0000

    FlacEncoder: Added fields and logic to track minimum and maximum encoded frame sizes.

--- src/io/nayuki/flac/encode/FlacEncoder.java
+++ src/io/nayuki/flac/encode/FlacEncoder.java
@@ -16,6 +16,8 @@ public final class FlacEncoder {
 	private final int sampleDepth;
 	private final int sampleRate;
 	private BitOutputStream out;
+	public int minFrameSize;  // In bytes, or -1 if no frames encoded
+	public int maxFrameSize;  // In bytes, or -1 if no frames encoded
 	
 	
 	public FlacEncoder(int[][] samples, int sampleDepth, int sampleRate, BitOutputStream out) throws IOException {
@@ -23,6 +25,8 @@ public final class FlacEncoder {
 		this.sampleDepth = sampleDepth;
 		this.sampleRate = sampleRate;
 		this.out = out;
+		minFrameSize = Integer.MAX_VALUE;
+		maxFrameSize = 0;
 		
 		out.writeInt(32, 0x664C6143);
 		out.writeInt(1, 1);
@@ -35,7 +39,15 @@ public final class FlacEncoder {
 			int n = Math.min(samples[0].length - pos, 4096);
 			long[][] subsamples = getRange(samples, pos, n);
 			FrameEncoder enc = FrameEncoder.computeBest(pos, subsamples, 16, sampleRate).encoder;
+			long startByte = out.getByteCount();
 			enc.encode(subsamples, out);
+			long frameSize = out.getByteCount() - startByte;
+			if (frameSize < 0 || (int)frameSize != frameSize)
+				throw new AssertionError();
+			if (minFrameSize == -1 || frameSize < minFrameSize)
+				minFrameSize = (int)frameSize;
+			if (maxFrameSize == -1 || frameSize > maxFrameSize)
+				maxFrameSize = (int)frameSize;
 			pos += n;
 		}
 	}
