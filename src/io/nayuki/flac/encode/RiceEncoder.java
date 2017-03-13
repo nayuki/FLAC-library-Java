eae8bbd2294826fa0c4d69148cf5615a5ae65718
Project Nayuki <me@nayuki.io>
2017-03-13 22:41:30 +0000

    Updated project URL in all header comments.

--- src/io/nayuki/flac/encode/RiceEncoder.java
+++ src/io/nayuki/flac/encode/RiceEncoder.java
@@ -2,7 +2,7 @@
  * FLAC library (Java)
  * 
  * Copyright (c) Project Nayuki
- * https://www.nayuki.io/
+ * https://www.nayuki.io/page/flac-library-java
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
